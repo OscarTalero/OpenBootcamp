5ed2c8f20fd46d54be122d21dc35db61f2f84ad5
OscarTalero <talero@gmail.com>
2022-05-13 11:56:05 -0500

    Tema4 ejercicio2

--- Python/Tema4/Ejercicio2.py
+++ Python/Tema4/Ejercicio2.py
@@ -1,7 +1,13 @@
-numero_inicial = 2
-numero_final = 8
+numero_inicial = int(input("Ingresa el numero inicial: "))
+numero_final = int(input("Ingresa el numero final: "))
+numeros_impares = []
+
+while numero_inicial > numero_final:
+    numero_inicial = int(input("El numero inicial debe ser menor. Ingresa otro numero12"))
 
 while numero_inicial < numero_final:
-    if numero_inicial % 2 == 1:
-        print(numero_inicial)
+    if (numero_inicial % 2) == 1:
+        numeros_impares.append(numero_inicial)
     numero_inicial +=1
+
+print(numeros_impares)
