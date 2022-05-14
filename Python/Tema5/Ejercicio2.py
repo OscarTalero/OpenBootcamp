91248d13c030c4b2b4e2dea8ac56fe9e45555456
OscarTalero <talero@gmail.com>
2022-05-14 11:58:19 -0500

    Tema5 Ejercicio1

---
+++ Python/Tema5/Ejercicio2.py
@@ -0,0 +1,9 @@
+numero = int(input('Ingrese un numero: '))
+
+if numero > 1:
+    for i in range(2,numero):
+        if numero % i == 0:
+            print('Numero ' + str(numero) + ' no es primo')
+            break
+    else:
+        print('Numero ' + str(numero) + ' es primo')
