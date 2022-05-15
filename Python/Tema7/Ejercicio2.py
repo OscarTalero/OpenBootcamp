f8b1ae8b5ff6d95d55e39a5299544204a5d05860
OscarTalero <talero@gmail.com>
2022-05-15 18:58:13 -0500

    Tema7 Ejercicio2

--- Python/Tema7/Ejercicio2.py
+++ Python/Tema7/Ejercicio2.py
@@ -1,16 +1,16 @@
 import time
 
 def main():
-    pass
-hora = int(time.strftime('%H'))
-minutos = int(time.strftime('%M'))
+    
+    hora = int(time.strftime('%H'))
+    minutos = int(time.strftime('%M'))
 
-if hora <  19:
-    hora = 18 - hora
-    minutos = 60 - minutos
-    print(f"Faltan {hora} horas y {minutos} minutos para salir")
-else:
-    print("Ya esa hora de salir")
+    if hora <  19:
+        hora = 18 - hora
+        minutos = 60 - minutos
+        print(f"Faltan {hora} horas y {minutos} minutos para salir")
+    else:
+        print("Ya esa hora de salir")
 
 
 if __name__ == '__main__':
