0a2f3a1038528b6297897e9ecaf470ac7ad29923
OscarTalero <talero@gmail.com>
2022-05-15 11:03:03 -0500

    Tema7 Ejercicio1

---
+++ Python/Tema7/Ejercicio1/main.py
@@ -0,0 +1,16 @@
+import calculadora as calc
+def main():
+    a = 6
+    b = 3
+    suma = calc.sumar(a,b)
+    resta = calc.restar(a,b)
+    multiplica = calc.multiplicar(a,b)
+    divide = calc.dividir(a,b)
+    print(f"La suma: {a} + {b} = {suma}")
+    print(f"La resta: {a} - {b} = {resta}")
+    print(f"La multiplicacion: {a} x {b} = {multiplica}")
+    print(f"La division de {a} / {b} = {divide}")
+
+
+if __name__ == '__main__':
+    main()
