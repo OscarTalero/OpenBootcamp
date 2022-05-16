dcac2eda5013e7a8b96a01853247cc77ebdb2fba
OscarTalero <talero@gmail.com>
2022-05-16 07:43:34 -0500

    Tema9 Ejercicio1

--- Python/Tema9/Ejercicio1.py
+++ Python/Tema9/Ejercicio1.py
@@ -1,15 +1,14 @@
 def main():
 
     paises = input("Ingrese los paises separados por coma: ")
-    listaPaises = set(paises.split(","))
-    paisesOrdenados = sorted(listaPaises)
+    paisesOrdenados = sorted(set(paises.split(",")))
 
     for pais in paisesOrdenados:
 
         print(f'{pais}, ', end = '')
 
     print('')
-    
+
 if __name__ == '__main__':
     main()
 
