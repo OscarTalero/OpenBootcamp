1abf1e500352eee9c0c6b43eb8cd48f91214a781
OscarTalero <talero@gmail.com>
2022-05-15 19:06:56 -0500

    Tema8 Ejercicio1

--- Python/Tema8/Ejercicio1.py
+++ Python/Tema8/Ejercicio1.py
@@ -1,6 +1,11 @@
 def main():
-    f = open('archivo.txt', w)
-    f = "datos"
+    
+    f = open('archivo.txt', 'w')
+    f.write("dato1\n")
+    f.close()
+
+    f = open('archivo.txt', 'a')
+    f.write("dato2\n")
     f.close()
 
 if __name__ == '__main__':
